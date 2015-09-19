87f0ffc655938008ec668f4701be494f49fc5e36
Azat Almeev <azat.almeev@gmail.com>
2015-09-20 00:05:22 +0300

    - Example commit

---
+++ FMManager/BridgingHeader.h
@@ -0,0 +1,17 @@
+//
+//  BridgingHeader.h
+//  FMManager
+//
+//  Created by Azat Almeev on 30.07.15.
+//  Copyright (c) 2015 Azat Almeev. All rights reserved.
+//
+
+#import <BlocksKit+UIKit.h>
+#import <BlocksKit+MessageUI.h>
+#import <MBProgressHUD.h>
+#import <MyHelpers.h>
+#import <Mantle.h>
+#import <ReactiveCocoa.h>
+#import "SwiftTryCatch.h"
+#import <MagicalRecord.h>
+#import <UIImageView+WebCache.h>
---
+++ FMManager/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  FMManager
+//
+//  Created by Azat Almeev on 30.07.15.
+//  Copyright (c) 2015 Azat Almeev. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //comment me to build successfully
+        let abc = NSString(string: "abc")
+    }
+
+}
